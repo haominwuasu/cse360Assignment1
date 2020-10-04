1e708475966b920d22aa3ecc91fdbe3f9e3443c8
Haomin <haomin1997@yahoo.com>
2020-10-04 07:06:27 -0700

    The second version

--- cse360_assignment_1/src/cse360assignment02/AddingMachine.java
+++ cse360_assignment_1/src/cse360assignment02/AddingMachine.java
@@ -2,31 +2,39 @@ package cse360assignment02;
 
 /*
  * 
- * @author Prof. Javier Gonzalez Sanchez
- * @version 1.0
- * @since 2020-09-22
+ * @author Haomin Wu
+ * @version 2.0
+ * @since 2020-10-03
  *
  */
 
 public class AddingMachine {
 	private int total;
-	  
+	private String record;  
+	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		this.record = "0";
 	}
 	  
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	  
 	public void add (int value) {
+		this.record = this.record + " + ";
+		this.record = this.record + String.valueOf(value);
+		this.total = this.total + value;
 	}
 
 	public void subtract (int value) {
+		this.record = this.record + " - ";
+		this.record = this.record + String.valueOf(value);
+		this.total = this.total - value;
 	}
 
 	public String toString () {
-		return "";
+		return this.record;
 	}
 
 	public void clear() {
